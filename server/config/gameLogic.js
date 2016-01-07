44f4ed17a4e51faa209e0bd03c92312ee7e64c55
Dakota-Peel <koteman624@gmail.com>
2016-01-07 15:29:35 -0800

    adding more game logic functions for voteing, reseting votes, lady check, number of players fro each quest, check for game over and win, check if each type of vote passed or failed

--- server/config/gameLogic.js
+++ server/config/gameLogic.js
@@ -1,5 +1,5 @@
 //this one command gernerates all game state details at start of game once passed in an array of player names.
-
+//gameInstance will refere to the whole game state object being passed in.
 var GameState = function (players) {
   this.allPlayersNames = players;
   this.gameOver = false;
@@ -19,7 +19,6 @@ var GameState = function (players) {
 };
 
 //ceates new player based on role and name passed in.
-
 var CreatePlayer = function (playerName, role) {
   this.name = playerName;
   this.role = role;
@@ -27,14 +26,13 @@ var CreatePlayer = function (playerName, role) {
   this.teamVote = null;
   this.questVote = null;
   this.isLeader = false;
-  this.onTeam = false;
+  this.onQuest = false;
   this.isLady = false;
   this.beenLady = false;
   this.specialAbility = null;
 };
 
 //assigns team good or bad based on role thats passed in.
-
 var assignTeam = function (role) {
   if (role === 'Assassin' || role === 'Morgona' || role === 'Mordred' || role === 'minion' || role === 'oberon') {
     return 'bad';
@@ -44,7 +42,6 @@ var assignTeam = function (role) {
 };
 
 //generates random list of roles based on number of players in game.
-
 var randomRoles = function (numberOfPlayers) {
   var goodRoles = ['Merlin', 'Percival', 'Servent', 'Servent', 'Servent' , 'Servent'];
   var badRoles = ['Assassin', 'Morgana', 'Mordred', 'Minion', 'Oberon'];
@@ -68,7 +65,6 @@ var randomRoles = function (numberOfPlayers) {
 };
 
 //assigns a random team leader for begining of game also assigns the lady of the lake to the player behind the leader.
-
 var assignLeader = function (gameInstance) {
   var randomIndex = Math.floor(Math.random()*gameInstance.numberOfPlayers);
   gameInstance.players[randomIndex].isLeader = true;
@@ -80,7 +76,6 @@ var assignLeader = function (gameInstance) {
 };
 
 //will rotate leader after each quest or team vote fail.
-
 var rotateLeader = function (gameInstance) {
   var index = null;
   for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
@@ -98,12 +93,221 @@ var rotateLeader = function (gameInstance) {
 };
 
 //will set players that are confirmed for a quest team. takes and input array of player names.
-
-var confirmQuestMembers = function (playerNames) {
-
+var confirmQuestMembers = function (gameInstance, playerNames) {
+  for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
+    if (playerNames.indexOf(gameInstance.players[i].name) !== -1) {
+      gameInstance.players[i].onQuest = true;
+    }
+  }
 };
 
-var test = new GameState(['dakota','kevin','kris','justin','hacker']);
-console.log(test);
-rotateLeader(test);
+//will reset all players onQuest property to false for new vote.
+var resetQuestMembers = function (gameInstance) {
+  for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
+    gameInstance.players[i].onQuest = false;
+  }
+};
+
+//reset all players team votes to null.
+var resetTeamVote = function (gameInstance) {
+  for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
+    gameInstance.players[i].teamVote = null;
+  }
+};
+
+//resets all players quest votes to null.
+var resetQuestVotes = function (gameInstance) {
+  for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
+    gameInstance.players[i].questVote = null;
+  }
+};
+
+//sets each player team vote property to true or false bassed on yes or
+//no input one player name and vote for each call. vote arrg is boolean.
+var setTeamVote = function (gameInstance, playerName, vote) {
+  for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
+    if (gameInstance.players[i].name === playerName){
+      gameInstance.players[i].teamVote = vote;
+    }
+  }
+};
+
+//sets quest votes for players that are on the quest team, one player at a time, vote is boolean
+var setQuestVote = function (gameInstance, playerName, vote) {
+  for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
+    if (gameInstance.players[i].name === playerName){
+      gameInstance.players[i].questVote = vote;
+    }
+  }
+};
+
+//takes input checkedPlayerName which is the player the lady decied to check.
+//function will return null if lady tries to check themself or someone that has
+//been the lady before. return team 'good' or 'bad' if a proper check can be completed.
+var ladyCheck = function (gameInstance, checkedPlayerName) {
+  for (var i =0; i < gameInstance.numberOfPlayers; i++) {
+    if (gameInstance.players[i].isLady === true) {
+      if (gameInstance.players[i].name === checkedPlayerName) {
+        //return null if lady trys to check herself.
+        return null;
+      }
+      gameInstance.players[i].beenLady = true;
+      gameInstance.players[i].isLady = false;
+    }
+  }
+  for (var j = 0; j < gameInstance.numberOfPlayers; j++) {
+    if (gameInstance.players[j].name === checkedPlayerName) {
+      if (gameInstance.players[j].beenLady === true) {
+        //can't check player that has already been lady before return null;
+        return null;
+      }
+      gameInstance.players[j].isLady = true;
+      return gameInstance.players[j].team;
+    }
+  }
+};
+
+//checks team vote outcome returns true if pass false if fail and null if not everyone voted.
+var teamVoteOutcome = function (gameInstance) {
+  var yesCount = 0;
+  var noCount = 0;
+  for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
+    if (gameInstance.players[i].teamVote === true) {
+      yesCount++;
+    } else if (gameInstance.players[i].teamVote === false) {
+      noCount++;
+    } else {
+      return null;
+    }
+  }
+  return yesCount > noCount;
+};
+
+//checks quest vote outcome returns true if pass false if fail and null if not all quest members voted.
+//also accounts for safety rounds that requier 2 fails to fail and noramlly only needs one fail to fail.
+var questVoteOutcome = function (gameInstance) {
+  var safety = false;
+  var fails = 0;
+  var passes = 0;
+  //checking if saftey round is needed
+  if (gameInstance.numberOfPlayers > 6 && gameInstance.questNumber === 4) {
+    safety = true;
+  }
+  for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
+    if (gameInstance.players[i].onQuest === true){
+      if (gameInstance.players[i].questVote === true) {
+        passes++;
+      } else if (gameInstance.players[i].questVote === false) {
+        fails++;
+      } else {
+        return null;
+      }
+    }
+  }
+  //if safety round check to see if there are at least 2 fails to fail
+  //else check to see is 0 fails for pass.
+  if (safety === true) {
+    return fails < 2;
+  } else {
+    return fails === 0;
+  }
+};
+
+//function is called when quest is  completed. It checks the outcome of the quest vote.
+//if vote was true from pass then it increases good wins if it was false from fail it increases bad wins.
+//function will return null if all votes were not valid. if was pass or fail then function increases quest counter
+//and checks for gameover and returns appropriate value of that function see below.
+var finishQuest = function (gameInstance) {
+  var questOutcome = questVoteOutcome(gameInstance);
+  gameInstance.questSet[gameInstance.questNumber]=questOutcome;
+  if (questOutcome === true) {
+    gameInstance.goodWins++;
+  } else if (questOutcome === false) {
+    gameInstance.badWins++;
+  } else {
+    return null;
+  }
+  gameInstance.questNumber++;
+  return checkGameOver(gameInstance);
+};
+
+//game over then will returun true if good won the game false if bad won the game and null
+// if theres no game over and continue playing;
+var checkGameOver = function (gameInstance) {
+  if (gameInstance.goodWins === 3) {
+    gameInstance.gameOver = true;
+    return true;
+  } else if (gameInstance.badWins === 3) {
+    gameInstance.gameOver = true;
+    return false;
+  } else {
+    return null;
+  }
+};
+
+//outputs the number of people required for current quest based on numbe rof players.
+var peopleNeededForQuest = function (gameInstance) {
+  var people = 2;
+  if (gameInstance.numberOfPlayers > 7) {
+    people++;
+  }
+  if (gameInstance.questNumber > 1) {
+    people++;
+  }
+  if (gameInstance.numberOfPlayers > 7 && gameInstance.questNumber > 3) {
+    people++;
+  }
+  if (gameInstance.numberOfPlayers === 5 && gameInstance.questNumber === 3) {
+    people--;
+  }
+  if (gameInstance.numberOfPlayers === 6) {
+    if (gameInstance.questNumber === 3 || gameInstance.questNumber === 5) {
+      people++;
+    }
+  }
+  if (gameInstance.numberOfPlayers === 7 && gameInstance.questNumber >3) {
+    people++;
+  }
+  return people;
+};
+
+
+//test for number of people in game function
+
+
+// var people = [];
+// var round =1;
+// for(var i = 5; i < 11; i++){
+//   for(var j = 0; j < i; j++){
+//     people.push(j);
+//   }
+//   var test = new GameState(people);
+
+//   while(round < 6){
+//     console.log('players', i, 'round', round, 'people needed' ,peopleNeededForQuest(test));
+//     round++;
+//     test.questNumber = round;
+//   }
+//   round = 1;
+//   people = [];
+// }
+
+//test for other functions
+
+//var test = new GameState(['six','dakota','kevin','kris','justin','hacker','seven']);
+// console.log(test);
+// rotateLeader(test);
+// test.questNumber = 4;
+// test.badWins = 2;
+// confirmQuestMembers(test, ['dakota','hacker','kevin']);
+// setQuestVote(test, 'dakota', true);
+// setQuestVote(test, 'kevin', false);
+// setTeamVote(test, 'justin', false);
+// setQuestVote(test, 'hacker', false);
+// setTeamVote(test, 'kris', true);
+// setTeamVote(test, 'six', true);
+// console.log(test.players);
+// console.log(ladyCheck(test, 'kevin'));
+// console.log(questVoteOutcome(test));
+// console.log(finishQuest(test));
 
