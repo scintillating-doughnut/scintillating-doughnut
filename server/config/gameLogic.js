27cbb56055fc5fdea00782d47bcdcf8f3c7d898e
Dakota-Peel <koteman624@gmail.com>
2016-01-07 12:13:17 -0800

    fixed some nameing conventions and typos, also added functions that chose random leader, rotate leader and assign lady of the lake according to leader

--- server/config/gameLogic.js
+++ server/config/gameLogic.js
@@ -7,14 +7,15 @@ var GameState = function (players) {
   this.questNumber = 1;
   this.goodWins = 0;
   this.badWins = 0;
-  this.teamVote = 1;
+  this.teamVoteFails = 1;
   this.numberOfPlayers = players.length;
-  this.avilableRoles = randomRoles(this.numberOfPlayers);
+  this.availableRoles = randomRoles(this.numberOfPlayers);
   this.questSet = {1 : null, 2 : null, 3 : null, 4 : null, 5 : null};
 
-  for (var i = 0; i < this.numberOfPlayers; i++){
-    this.players.push(new CreatePlayer(this.allPlayersNames[i],this.avilableRoles[i]));
+  for (var i = 0; i < this.numberOfPlayers; i++) {
+    this.players.push(new CreatePlayer(this.allPlayersNames[i],this.availableRoles[i]));
   }
+  assignLeader(this);
 };
 
 //ceates new player based on role and name passed in.
@@ -25,7 +26,7 @@ var CreatePlayer = function (playerName, role) {
   this.team = assignTeam(this.role);
   this.teamVote = null;
   this.questVote = null;
-  this.leader = false;
+  this.isLeader = false;
   this.onTeam = false;
   this.isLady = false;
   this.beenLady = false;
@@ -34,10 +35,10 @@ var CreatePlayer = function (playerName, role) {
 
 //assigns team good or bad based on role thats passed in.
 
-var assignTeam = function(role){
-  if(role === 'Assassin' || role === 'Morgona' || role === 'Mordred' || role === 'minion' || role === 'oberon'){
+var assignTeam = function (role) {
+  if (role === 'Assassin' || role === 'Morgona' || role === 'Mordred' || role === 'minion' || role === 'oberon') {
     return 'bad';
-  }else{
+  } else {
     return 'good';
   }
 };
@@ -45,8 +46,8 @@ var assignTeam = function(role){
 //generates random list of roles based on number of players in game.
 
 var randomRoles = function (numberOfPlayers) {
-  var goodRoles = ['Merlin', 'Percival', 'servent', 'servent', 'servent' , 'servent'];
-  var badRoles = ['Assassin', 'Morgona', 'Mordred', 'minion', 'oberon'];
+  var goodRoles = ['Merlin', 'Percival', 'Servent', 'Servent', 'Servent' , 'Servent'];
+  var badRoles = ['Assassin', 'Morgana', 'Mordred', 'Minion', 'Oberon'];
   var playerSet = [];
   var randomPlayerSet = [];
   var numBad = Math.ceil(numberOfPlayers/3);
@@ -66,5 +67,43 @@ var randomRoles = function (numberOfPlayers) {
   return randomPlayerSet;
 };
 
-console.log(new GameState(['dakota','kevin','kris','justin','hacker']));
+//assigns a random team leader for begining of game also assigns the lady of the lake to the player behind the leader.
+
+var assignLeader = function (gameInstance) {
+  var randomIndex = Math.floor(Math.random()*gameInstance.numberOfPlayers);
+  gameInstance.players[randomIndex].isLeader = true;
+  if (randomIndex !== 0) {
+    gameInstance.players[randomIndex-1].isLady = true;
+  } else {
+    gameInstance.players[gameInstance.numberOfPlayers-1].isLady =true;
+  }
+};
+
+//will rotate leader after each quest or team vote fail.
+
+var rotateLeader = function (gameInstance) {
+  var index = null;
+  for (var i = 0; i < gameInstance.numberOfPlayers; i++) {
+    if (gameInstance.players[i].isLeader === true) {
+      index = i;
+      gameInstance.players[i].isLeader = false;
+    }
+  }
+  if (index === gameInstance.numberOfPlayers-1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  gameInstance.players[index].isLeader = true;
+};
+
+//will set players that are confirmed for a quest team. takes and input array of player names.
+
+var confirmQuestMembers = function (playerNames) {
+
+};
+
+var test = new GameState(['dakota','kevin','kris','justin','hacker']);
+console.log(test);
+rotateLeader(test);
 
