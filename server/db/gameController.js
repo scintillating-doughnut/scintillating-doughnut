a43cb249fa836a0be8b928a8640405eadcfe8381
Justin Shaw <jshaw22@gmail.com>
2016-01-06 21:31:25 -0800

    Database models and controllers set up

---
+++ server/db/gameController.js
@@ -0,0 +1,28 @@
+var Gamestats = require('./gameModel.js');
+    Q = require('q');
+
+var getStats = Q.nbind(Gamestats.find. Gamestats);
+var addStats = Q.nbind(Gamestats.create, Gamestats);
+
+module.exports = {
+	allStats: function (req, res) {
+		getStats({})
+		.then(function (stats) {
+			res.json(stats);
+		})
+		.fail(function (err) {
+			next(error);
+		})
+	},
+
+	storeFinishedGameStats: function (req, res, next) {
+		return addStats({
+			spyWin: req.body.spyWin,
+			spyLose: req.body.spyLose,
+			numPlayers: req.body.numPlayers,
+			gameTime: req.body.gameTime,
+			numRoundsPlayed: req.body.numRoundsPlayed
+		})
+
+	}
+}
