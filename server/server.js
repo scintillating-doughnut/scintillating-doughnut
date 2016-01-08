08ba71804a08d23ea2ff52bc6c54165a7e5b6b92
Kris Albert Lee <krisalbert@gmail.com>
2016-01-07 18:30:08 -0800

    Rewrite server to interact with gameLogic

--- server/server.js
+++ server/server.js
@@ -3,40 +3,20 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var stats = require()
-var gameController = require('gameController.js');
+var gameController = require('./db/gameController.js');
+var gameLogic = require('./config/gameLogic.js');
 
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/client'));
-app.use(bodyParser.json());
-
-mongoose.connect('mongodb://localhost/donut');
-
-//=========================================
-
-//initial request to '/' will just be sent to '/'
-//front-end will deal with serving desired view
-app.get('/', function(req, res) {
-  res.send('/');
-});
-
-//intial
-app.post('/api/players/:id', function (req, res) {
-  console.log("ID", req.params.id);
-
-
-});
-
-app.get('/api/stats', function (req, res) {
-  gameController.allStats();  
-});
-
-app.post('api/stats', function (req, res) {
-
-});
+//array of current players, will get populated as players
+//join the game
+var currentPlayers = [];
+var questMembers = [];
+var readyCounter = 0;
+var currentGame;
+var teamVoteCounter = 0;
+var questVoteCounter = 0;
+var currentQuestSize = 0;
 
 //this function logs all the get and post requests made to
 //the server.
@@ -44,13 +24,122 @@ var morganLogger = function (app, express) {
  app.use(morgan('dev'));
  app.use(bodyParser.urlencoded({extended: true}));
  app.use(bodyParser.json());
- app.use(express.static(__dirname + '/../../client'));
+ app.use(express.static('client'));
 };
 
 morganLogger(app, express);
 
+//starts the express server
 var server = app.listen(port, function () {
   console.log('Server listening at port ', port);
 });
 
+// use socket.io with express server
+var io = require('socket.io').listen(server);
+
+io.on('connection', function (client) {  
+  console.log('Client connected...');
+
+  //on hearing enterPlayerName event, push player name
+  //to array
+  client.on('enterPlayerName', function (data) {
+    currentPlayers.push(data);
+    console.log(data);
+    client.emit('messages', data +' added ');
+  });
+
+  //listens to ready event from each player
+  client.on('ready', function (name) {
+    //increases readyCounter each time a player transmit event
+    readyCounter++;
+
+    //if readyCounter is equal to # of players, pass currentPlayers
+    //to gameLogic to start game
+    if(readyCounter === currentPlayers.length){
+      currentGame = new gameLogic.GameState(currentPlayers);
+      client.emit('game-state-ready', currentGame);
+    } else {
+      client.emit('game-state-notReady', 'Not Ready' );
+    }
+  });
+
+  client.on('teamPlayerVote', function (data) {
+    //increase teamVoteCounter for every vote that I get
+    teamVoteCounter++
+    
+    //send each playerVote to gameLogic with the currentGame
+    gameLogic.setTeamVote(currentGame, data.name, data.teamVote)
+
+    //this will happen when each player has voted
+    if(teamVoteCounter===currentPlayers.length){
+      //send to gameLogic to canvas votes, game logic will
+      //return if vote passed or not
+      var result = gameLogic.teamVoteOutcome(currentGame);
+
+      //if team vote passes, send quest members to gameLogic
+      if(result){
+        gameLogic.confirmQuestMembers(currentGame, questMembers);
+      //if team vote fails, reset quest members and increase
+      //vote fails
+      } else {
+        gameLogic.resetQuestMembers(currentGame);
+        questMembers = [];
+        currentGame.teamVoteFails++;
+        gameLogic.checkGameOver(currentGame);
+      }
+      //emit state of current state to object
+      client.emit('game-state', currentGame);
+
+      //clear teamVoteCounter to 0 for the next team vote
+      teamVoteCounter = 0;
+
+    } else {
+      client.emit('game-state', "not ready");
+    }
+
+  });
+
+  client.on('teamQuestVote', function (data) {
+    //increase quest vote 
+    questVoteCounter++;
+
+    gameLogic.setQuestVote(currentGame, data.name, data.questVote)
+    
+    if(questVoteCounter === currentGame.questSize){
+      var result = gameLogic.questVoteOutcome(currentGame);
+      gameLogic.finishQuest(currentGame);
+
+      //sends 
+      client.emit('game-state', result)
+
+      //resets questVoteCounter to 0
+      questVoteCounter = 0;
+    }
+
+
+  });
+
+  client.on('confirmQuestMembers', function (names) {
+    //send playerName and currentGame to gameLogic
+    for(var name in names){
+      questMembers.push(name);
+    }
+
+    //send game state object to client
+    client.emit('game-state-ready', currentGame);
+
+  });
+
+  // client.on('questSize', function (names) {
+  //   //ask gameLogic for number of quest members needed
+  //   currentQuestSize = gameLogic.peopleNeededForQuest(currentGame);
+  //   client.emit('questSizeReply', size);
+  // });
+
+
+});
+
+// app.get('/api/stats', gameController.allStats);
+
+// app.post('api/stats', gameController.addGameStats);
 
